72b98850b27a7d3a43912b9810475491d8de0392
soroosh bagherieh <soroosh1231@outlook.com>
2024-09-03 14:28:31 +0330

    Add project files.

---
+++ ChatRoomStoryTeller/ChatItem.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatRoomStoryTeller
+{
+    /// <summary>
+    /// Interaction logic for ChatItem.xaml
+    /// </summary>
+    public partial class ChatItem : UserControl
+    {
+        public string Text { get { return txtMessage.Text; } set {  txtMessage.Text = value; } }    
+        public string SendedTime { get { return lblTime.Content.ToString(); } set {  lblTime.Content = value; } }    
+        public Senders sender { set { if(value==Senders.me)
+                { brdr.Background= (SolidColorBrush)new BrushConverter().ConvertFrom("#7289da");
+                    txtMessage.HorizontalAlignment= HorizontalAlignment.Right;
+                }
+        else
+                {
+                    brdr.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#282B30");
+                    txtMessage.HorizontalAlignment = HorizontalAlignment.Left;
+                }    
+                        } }
+        public ChatItem()
+        {
+            InitializeComponent();
+            this.Height=txtMessage.Height + lblTime.Height;
+                this.Width = txtMessage.Width;
+
+        }
+    }
+    public enum Senders
+    {
+        me,
+        you
+    }
+}
---
+++ ChatRoomStoryTeller/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatRoomStoryTeller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 50; i++)
+            {
+                stackPnlUsers.Children.Add(new UserinList()
+                {
+                    Margin = new Thickness(10),
+                    Width = 370,
+                    Height = 80,
+                    Username = $"UserName{i}",
+                    ChatPreview = "LoremIpsum LoremIpsum LoremIpsum",
+                    ProflePic = new BitmapImage(new Uri(Environment.CurrentDirectory+"/profile.jpg", UriKind.Relative))
+
+                });
+                var temp = ((UserinList)stackPnlUsers.Children[stackPnlUsers.Children.Count - 1]);
+                temp.MouseDown += User_MouseDown;
+
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if(i%2==0)
+                stckPnlChats.Children.Add(new ChatItem() { Margin = new Thickness(10), Text = "lorem ipsum hello" , HorizontalAlignment=HorizontalAlignment.Right , SendedTime =$"10:0{i}",  sender=Senders.me });
+                else
+                    stckPnlChats.Children.Add(new ChatItem() { Margin = new Thickness(10), Text = "lorem ipsum hello", HorizontalAlignment = HorizontalAlignment.Left, SendedTime = $"10:0{i}", sender = Senders.you });
+
+            }
+            scrlChat.ScrollToEnd();
+        }
+        bool tempbool = true;
+        private void User_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var temp = ((UserinList)sender);
+            if (tempbool)
+            {
+                temp.SetAsNotReaded();
+                tempbool = false;
+            }
+            else
+            {
+                temp.SetAsReaded();
+                tempbool = true;
+            }
+
+        }
+
+        private void Send_clicked(object sender, MouseButtonEventArgs e)
+        {
+            stckPnlChats.Children.Add(new ChatItem() { Margin = new Thickness(10), Text = txtToSend.Text, HorizontalAlignment = HorizontalAlignment.Right, SendedTime = DateTime.Now.ToShortTimeString(), sender = Senders.me });
+            txtToSend.Clear();
+            scrlChat.ScrollToEnd();
+        }
+    }
+}
